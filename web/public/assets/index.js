fdf994f88ae3eeb4862281e7fe65483a5b79144a
hma <hma@thoughtworks.com>
2017-07-27 09:04:01 +0800

    fix the web env

---
+++ web/public/assets/index.js
@@ -0,0 +1,36 @@
+webpackJsonp([0],{
+
+/***/ 83:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 84:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _react = __webpack_require__(33);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = __webpack_require__(32);
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+__webpack_require__(83);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_reactDom2.default.render(_react2.default.createElement(
+  'h1',
+  null,
+  'Hello,World!'
+), document.getElementById('app'));
+
+/***/ })
+
+},[84]);
